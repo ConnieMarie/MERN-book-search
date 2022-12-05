8c8f7a949f3b7427486a93d7e7971c2a9e2552bd
conniemarie <constance.marie.strahan@gmail.com>
2022-12-05 16:57:36 -0600

    back-end/Schemas/typeDefs and resolvers

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,70 @@
+// import user model
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken } = require("../utils/auth");
+const { AuthenicaitonError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+
+        return userData;
+      }
+      throw new AuthenicaitonError("You are not logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.input } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenicaitonError("You are not logged in!");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenicaitonError("You are not logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
