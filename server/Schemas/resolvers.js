4debd334fb76a15c6046fc9f3007d13fe90eec94
conniemarie <constance.marie.strahan@gmail.com>
2022-12-07 19:17:35 -0600

    remove console.log

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -26,8 +26,7 @@ const resolvers = {
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-      console.log('===========')
-      console.log(user)
+      
       if (!user) {
         throw new AuthenticationError("Incorrect credentials");
       }
